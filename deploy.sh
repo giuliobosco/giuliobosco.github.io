c4c60ab4e671d086454158cecf56d4558d86bfdc
giuliobosco <giuliobva@gmail.com>
2020-08-06 13:39:47 +0200

    fix deploy check worktree

--- deploy.sh
+++ deploy.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+if [ ! -d "./public" ]; then
+    git pull origin gh-pages
+    git branch –track gh-pages
+    git worktree add -B gh-pages public gh-pages
+fi
+
 hugo
 cd public && git add --all && git commit -m "Publishing to gh-pages" && git push && cd ..
